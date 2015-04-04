c7d4686229b6ed1f2079845ea1d0d2600d8cb4db
kasrakoushan <kasra.koushan@gmail.com>
2015-04-03 20:29:49 -0400

    Fixed up documentation and style

--- strategy_minimax_memoize.py
+++ strategy_minimax_memoize.py
@@ -1,17 +1,25 @@
 from strategy import Strategy
 
+
 class StrategyMinimaxMemoize(Strategy):
-    ''' minimax strategy '''
+    ''' Interface for suggesting moves
     
-    states_data = {}
+    Uses memoizing minimax algorithm that stores scores of GameStates in a
+    dictionary to avoid redunancies in expanding the game tree
+    
+    DATA: dict  -- dictionary storing scores of GameStates
+    '''
+    
+    DATA = {}
     
     def __init__(self, interactive=False):
-        ''' (StrategyMinimaxMemoize) -> None
+        ''' (StrategyMinimaxMemoize, bool) -> None
         
         Initialize a minimax memoize strategy
         '''
         
-        StrategyMinimaxMemoize.states_data = {}
+        # empty the dictionary to avoid overlap of memory between games
+        StrategyMinimaxMemoize.DATA = {}
     
     def suggest_move(self, state):
         ''' (StrategyMinimaxMemoize, GameState) -> Move 
@@ -20,55 +28,48 @@ class StrategyMinimaxMemoize(Strategy):
         Override Strategy.suggest_move
         '''
         
-        return self.minimax(state)[1]
-    
-    def minimax(self, state):
-        ''' (StrategyMinimaxMemoize, GameState) -> (int, Move)
-        
-        Suggest a move using result method
-        Return the best score and the move that achieves it
-        '''
-        
         # make a list of options
         moves = state.possible_next_moves()
         
         # find opponent's score for each option, multiply by (-1)
-        scores = [(-1)*self.result(state.apply_move(i)) 
+        scores = [(-1) * self.minimax(state.apply_move(i)) 
                   for i in state.possible_next_moves()]
         
         # find the maximum score available
         score = max(scores)
         
-        return (score, moves[scores.index(score)])
+        return moves[scores.index(score)]
     
-    def result(self, state):
-        ''' (StrategyMinimax, GameState) -> int
+    def minimax(self, state):
+        ''' (StrategyMinimax, GameState) -> float
         
-        Return score for the given game state – how favourable the game is
-        for the next_player
+        Return score for the given game state, that is, how favourable the 
+        game is for the next_player
         
         If a winning strategy is available, return 1
         Elif a tying strategy is available, return 0
         Else return -1
+        
+        While game tree is traversed, store scores for GameStates in DATA 
         '''
         
-        if repr(state) in StrategyMinimaxMemoize.states_data:
+        if repr(state) in StrategyMinimaxMemoize.DATA:
             # if game is known, return value from dictionary
-            return StrategyMinimaxMemoize.states_data[repr(state)]            
+            return StrategyMinimaxMemoize.DATA[repr(state)]            
         elif state.over:
             # if game is over, return outcome, assign value to dictionary
-            StrategyMinimaxMemoize.states_data[repr(state)] = state.outcome()
+            StrategyMinimaxMemoize.DATA[repr(state)] = state.outcome()
             return state.outcome()
         else:
             # if game is not over, run through all possible_next_moves
             states = [state.apply_move(i) 
                       for i in state.possible_next_moves()]
             
-            # for each new hypothetical state, find (-1)*result
-            # multiplied by (-1) since result is opponent's score
-            scores = [(-1)*self.result(i) for i in states]
+            # for each new hypothetical state, find (-1) * score
+            # multiplied by (-1) since miminax returns opponent's score
+            scores = [(-1) * self.minimax(i) for i in states]
             
-            # return the maximum of the scores for each result
+            # return the maximum of the scores for each move
             # assign this value to dictionary
-            StrategyMinimaxMemoize.states_data[repr(state)] = max(scores)
+            StrategyMinimaxMemoize.DATA[repr(state)] = max(scores)
             return max(scores)
